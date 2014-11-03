a54f66976c9f4f519a559da57572c6532f3a0a68
zhaozhiming <kingzzm1982@sina.com>
2014-11-03 22:50:41 +0800

    add repository

--- src/main/java/com/farmer/baton/repo/FarmerMapper.java
+++ src/main/java/com/farmer/baton/repo/FarmerMapper.java
@@ -1,9 +1,11 @@
 package com.farmer.baton.repo;
 
 import com.farmer.baton.model.Farmer;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FarmerMapper {
     List<Farmer> findAll();
 }
